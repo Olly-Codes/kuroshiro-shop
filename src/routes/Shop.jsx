e32355e59de74c754d97ce895df36d3abf5ad5e5
Olly <ollycodesdev@gmail.com>
2026-06-27 16:57:57 +0200

    feat: add rendering of products to Shop component

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import dummyProducts from '../products.json';
 
 const Shop = () => {
-  return (
-    <div className="shop-content">Shop</div>
-  )
+    const [products, setProducts] = useState(dummyProducts);
+
+    return (
+        <div className="shop-content">
+            <h1>The Collection</h1>
+            <ul className="product-wrapper">
+                {dummyProducts.map((product) => {
+                    return <li key={product.id}>
+                        <div className="product-img">
+                            <img style={{ width: 200, height: 200 }} src={product.image} alt={product.title} />
+                        </div>
+                        <div className="product-details">
+                            <p>{product.category}</p>
+                            <p>{product.title}</p>
+                            <div className="price-rating">
+                                <p>R{product.price}</p>
+                                <p>{product.rating.rate}</p>
+                            </div>
+                        </div>
+                        <div className="input-wrapper">
+                            <button type="button">-</button>
+                            <input type="text" inputMode="numeric" />
+                            <button type="button">+</button>
+                        </div>
+                        <div className="button-wrapper">
+                            <button type="button">Add to Cart</button>
+                        </div>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default Shop;
