c65d0891d619cbce05e282db58d42ac881ef5d6e
Olly <ollycodesdev@gmail.com>
2026-06-28 15:00:01 +0200

    refactor: products state array for product quantities

--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -2,18 +2,22 @@ import { useState } from 'react';
 import dummyProducts from '../products.json';
 
 const Shop = () => {
-    const [products, setProducts] = useState(dummyProducts);
-    const [inputValue, setInputValue] = useState(0);
+    const [products, setProducts] = useState(() => {
+        return dummyProducts.map((product) => {
+            return { ...product, quantity: 0 }
+        });
+    });
+    // const [inputValue, setInputValue] = useState(0);
 
     return (
         <div className="shop-content">
             <h1>The Collection</h1>
             <ul className="product-wrapper">
-                {dummyProducts.map((product) => {
+                {products.map((product) => {
                     return <li key={product.id}>
-                        <div className="product-img">
+                        {/* <div className="product-img">
                             <img style={{ width: 200, height: 200 }} src={product.image} alt={product.title} />
-                        </div>
+                        </div> */}
                         <div className="product-details">
                             <p>{product.category}</p>
                             <p>{product.title}</p>
@@ -27,8 +31,8 @@ const Shop = () => {
                             <input 
                                 type="text" 
                                 inputMode="numeric"
-                                value={inputValue}
-                                onChange={(e) => setInputValue(e.target.value)}
+                                value={products.find((p) => p.id === product.id).quantity}
+                                // onChange={(e) => setInputValue(e.target.value)}
                                 style={{ textAlign: 'center' }}
                              />
                             <button type="button">+</button>
