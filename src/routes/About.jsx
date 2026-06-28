71145b62a29596334dacaf6ba614a1ea25e2d329
Olly <ollycodesdev@gmail.com>
2026-06-28 20:07:47 +0200

    feat: add content to about page component

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,36 @@
 
 const About = () => {
   return (
-    <div className="about-content">About</div>
+    <div className="about-content">
+      <section className="main-about-section">
+        <div className="main-about-left">
+          <h1>We make pieces worth owning.</h1>
+        </div>
+
+        <div className="main-about-right">
+          <p>KuroShiro combines two worlds that seemingly do not connect and make it work through luxury.</p>
+        </div>
+      </section>
+
+      <section className="middle-about-section">
+        <p>The rule</p>
+        <p>It's simple. We produce a limited amount of pieces. There no reprints or restocks. Get it while it's hot.</p>
+      </section>
+
+      <section className="bottom-about-section">
+        <ul className="about-details">
+          <li>
+            <p>Black and white work well because everything else is noise. A bit of gold works because some things are worth more.</p>
+          </li>
+          <li>
+            <p>There are no exceptions. Every print has a number recorded and each piece limited. That's what makes us the standard</p>
+          </li>
+          <li>
+            <p>Season 45. 20 high end pieces. Once this run ends or we are out of stock, that is it. No second chances.</p>
+          </li>
+        </ul>
+      </section>
+    </div>
   )
 }
 
