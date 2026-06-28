5a1d34106b26661f58a6907193ddef4027ba7600
Olly <ollycodesdev@gmail.com>
2026-06-28 19:04:22 +0200

    feat: add order summary section

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,41 +1,60 @@
 
 const Cart = ({ cart, handleIncrementCart, handleDecrementCart }) => {
+  const subtotal = Number((cart.reduce((subtotal, product) => subtotal + (product.quantity * product.price), 0)).toFixed(2));
+  const tax = Number((cart.reduce((subtotal, product) => subtotal + (product.quantity * product.price), 0) * 0.15).toFixed(2));
+  const total = Number((subtotal + tax).toFixed(2));
   return (
     <div className="cart-content">
       <h1>Your Cart</h1>
-      <div className="cart-content">
-        <ul>
-          {cart.map((product) => {
-            return <li key={product.id}>
-              {/* <div className="cart-img-wrapper">
-                <img src={product.image} alt={product.title} />
-              </div> */}
-              <div className="cart-product-info-wrapper">
-                <p>{product.category}</p>
-                <p>{product.title}</p>
-                <div className="cart-buttons-wrapper">
-                  <button 
-                    className="cart-decrement-btn"
-                    onClick={() => handleDecrementCart(product.id)}
-                  >
-                    -
-                  </button>
-                  <span>{product.quantity}</span>
-                  <button 
-                    className="cart-increment-btn"
-                    onClick={() => handleIncrementCart(product.id)}
-                  >
-                    +
-                  </button>
+      <div className="cart-details">
+        <div className="cart-items">
+          <ul>
+            {cart.map((product) => {
+              return <li key={product.id}>
+                {/* <div className="cart-img-wrapper">
+                  <img src={product.image} alt={product.title} />
+                </div> */}
+                <div className="cart-product-info-wrapper">
+                  <p>{product.category}</p>
+                  <p>{product.title}</p>
+                  <div className="cart-buttons-wrapper">
+                    <button 
+                      className="cart-decrement-btn"
+                      onClick={() => handleDecrementCart(product.id)}
+                    >
+                      -
+                    </button>
+                    <span>{product.quantity}</span>
+                    <button 
+                      className="cart-increment-btn"
+                      onClick={() => handleIncrementCart(product.id)}
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
-              </div>
-              <div className="cart-price-btn-wrapper">
-                <p>{product.price}</p>
-                <button>Remove</button>
-              </div>
-            </li>
-          })}
-        </ul>
+                <div className="cart-price-btn-wrapper">
+                  <p>{product.price}</p>
+                  <button>Remove</button>
+                </div>
+              </li>
+            })}
+          </ul>
+        </div>
+
+        <div className="cart-summary">
+          <h2>Order summary</h2>
+          <ul>
+            <li>Subtotal <span>R {subtotal}</span></li>
+            <li>Shipping <span>Free</span></li>
+            <li>Tax <span>R {tax}</span></li>
+            <li>Total <span>R {total}</span></li>
+          </ul>
+
+          <div className="cart-checkout-btn-wrapper">
+            <button type="button">Proceed to checkout</button>
+          </div>
+        </div>
       </div>
     </div>
   )
